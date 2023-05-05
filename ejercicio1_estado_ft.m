d63b52f2a4eb67eeee1a0934f0e6bad7fea873ba
Patricio Arévalo <89281750+matias-25@users.noreply.github.com>
2023-05-05 04:45:56 -0300

    Update ejercicio1_estado_ft.m

--- ejercicio1_estado_ft.m
+++ ejercicio1_estado_ft.m
@@ -1,9 +1,16 @@
-%Aqui conviertea de la matriz de las ecuaciones de estados a la funcion de
-%trnferencia
+%Aqui convierte del espacio de estados a la funcion de
+%transferencia
 R=4700; L=10^(-5); C=100*10^(-9);
 syms s G
 A=[-R/L -1/L; 1/C 0];
 B=[1/L;0];
 C=[R 0];
-Maux=inv(s*eye(2)-A);
-G=(C*Maux*B)
+D=0;
+Maux=(s*eye(2)-A)^(-1);
+G=(C*Maux*B);
+disp(G); %este resultado es usando la fórmula
+
+[num,den] = ss2tf(A,B,C,D);
+G_1=tf(num,den);
+disp(G_1); %este resultado es usando la fucion de MatLab
+disp(roots(den));
