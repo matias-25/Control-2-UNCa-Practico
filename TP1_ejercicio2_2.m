f04f886496e6572aeaaeadf3791a381121a1623d
Patricio Arévalo <89281750+matias-25@users.noreply.github.com>
2023-06-07 21:33:19 -0300

    Add files via upload

---
+++ TP1_ejercicio2_2.m
@@ -0,0 +1,35 @@
+clear;close all;
+X=-[0; 0 ; 0; 0];ii=0;t_etapa=1e-6;tF=5;
+IaMax=0;
+TL=1.4007e-3; 
+u=12; 
+for t=0:t_etapa:tF
+    ii=ii+1;
+    X=modmotor_2_1(t_etapa, X,[u,TL]);
+    x1(ii)=X(1);%ia
+    x2(ii)=X(2);%Omega velocidad angular
+    x3(ii)=X(3);%wp aceleracion angular
+    x4(ii)=X(4);%tita, angulo de salida
+%     acc(ii)=u;%u escalón entrada
+%     accTL(ii)=TL;%escalon Torque
+    
+        if x1(ii)>IaMax
+        IaMax=x1(ii); %Corriente armadura máxima
+        end
+end
+t=0:t_etapa:tF;
+
+fprintf('Corriente armadura Max %d [A],con torque max de carga %d [N*m]',IaMax,TL);
+subplot(4,1,1);
+plot(t,x1);title('Corriente i_a');
+subplot(4,1,2);
+plot(t,x2);title(' \omega_t');
+subplot(4,1,3);
+plot(t,x3);title('Salida wp');
+subplot(4,1,4);
+plot(t,x4);title('Salida \theta_t');
+% subplot(6,1,5);
+% plot(t,acc);title('Entrada v_a');
+% subplot(6,1,6);
+% plot(t,accTL);title('Torque de Carga');
+xlabel('Tiempo [Seg.]');
